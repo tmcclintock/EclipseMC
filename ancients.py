e198cadd5fc2df7a5c705f71c88b20c53fc3622d
Tom McClintock <tmcclintock@email.arizona.edu>
2016-01-04 11:35:38 -0700

    Wrote dice.py and ship.py. Wrote an example battle simulation that is very rough. the first ship to be designed will be the ancients, but the ship.py file is probably not complete.

---
+++ ancients.py
@@ -0,0 +1,3 @@
+"""
+This contains classes for ancient ships.
+"""
---
+++ dice.py
@@ -0,0 +1,28 @@
+"""
+This contains a six sided dice object.
+"""
+
+import random
+
+class Dice(object):
+
+    def __init__(self,color="Yellow",value=0):
+        self.color = color
+        self.value = value
+
+    def __str__(self):
+        if self.value==0:
+            return "%s dice has not been rolled yet"%self.color
+        return "%s dice rolled a %d"%(self.color,self.value)
+
+    def Roll(self):
+        self.value = random.randint(1,6)
+        return self
+    
+if __name__ == '__main__':
+    testdice = Dice("Red")
+    print testdice
+    testdice.Roll()
+    print testdice
+    testdice = Dice().Roll()
+    print testdice
